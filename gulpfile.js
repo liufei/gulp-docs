300cf85ff5591274cfc152113f425c9b6f998f25
biped <poticr@gmail.com>
2015-09-11 12:14:59 +0800

    automate build and deploy by using git hooks and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', function() {
+gulp.task('build', function (cb) {
+  exec('gitbook build', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('deploy', ['build'], function () {
   return gulp.src('./_book/**/*')
     .pipe(ghPages());
 });
