0d41f816c66e441946baf0efedc2d8039d91577e
biped <poticr@gmail.com>
2018-08-13 11:28:06 +0800

    Add edit link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  head: [['link', { rel: 'icon', href: '/favicon.png' }]],
-  ga: 'UA-36637973-3',
   locales: {
     '/': {
       lang: 'en-US',
@@ -8,34 +6,44 @@ module.exports = {
       description: 'Unofficial site for better reading experience.',
     },
     '/zh/': {
-      lang: 'zh-Hans',
+      lang: 'zh-CN',
       title: 'Gulp 中文文档',
       description: '为了更好的阅读体验而创建的Gulp中文文档。',
     },
   },
+  head: [['link', { rel: 'icon', href: '/favicon.png' }]],
+  ga: 'UA-36637973-3',
   themeConfig: {
+    repo: 'bipedd/gulp-docs',
+    editLinks: true,
+    docsDir: 'docs',
     locales: {
       '/': {
-        selectText: 'Languages',
         label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
         nav: [
           { text: 'Getting Started', link: '/getting-started' },
           { text: 'API', link: '/API' },
+          { text: 'CLI', link: '/CLI' },
           { text: 'Recipes', link: '/recipes/' },
           { text: 'Writing a plugin', link: '/writing-a-plugin/' },
         ],
       },
       '/zh/': {
-        selectText: '选择语言',
         label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
         nav: [
           { text: '入门指南', link: '/zh/getting-started' },
           { text: 'API', link: '/zh/API' },
+          { text: 'CLI', link: '/zh/CLI' },
           { text: '秘籍', link: '/zh/recipes/' },
           { text: '编写插件', link: '/zh/writing-a-plugin/' },
         ],
       },
     },
-    repo: 'bipedd/gulp-docs',
   },
 }
